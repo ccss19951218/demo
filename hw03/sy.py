08fdc1edde8b7b9002f9b2f5b64fcde2e58737f4
people775 <2677075598@qq.com>
2016-04-25 18:57:56 +0800

    hw03

---
+++ hw03/sy.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+guess_me = 7
+
+
+
+
+if guess_me > 7:
+
+
+    print('Too hight')
+
+
+
+elif guess_me < 7:
+
+
+    print('Too low')
+
+
+else:
+
+    print('Just right')
+
+
+    
+
+
+
+    
---
+++ hw03/sy2.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+guess_me = 7
+
+
+
+start = 1
+
+
+while True:
+
+
+
+
+    if start < guess_me:
+
+        print("Too low")
+
+    elif start > guess_me:
+
+         print("oops")
+
+         break
+
+    else:
+
+        print("Found it!")
+
+        break
+    
+    start = start+1
